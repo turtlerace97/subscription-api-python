ee8c81410f7a63ba513dfc57f45d9349727a003c
soonyeop hong <soonyeophong@MacBook-Pro.local>
2024-07-25 15:36:45 +0900

    docs : add html page

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import uvicorn
 
 from app.conf.config import Config
@@ -7,12 +9,18 @@ from app.database.conn import close_db_connection, connect_to_db
 from app.routes import subscription
 
 
+templates = Jinja2Templates(directory="static")
+
 app = FastAPI()
 
 # db
 app.add_event_handler("startup", Config.app_setting_validate)
 app.add_event_handler("startup", connect_to_db)
 app.add_event_handler("shutdown", close_db_connection)
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 app.include_router(
     subscription.router, 
